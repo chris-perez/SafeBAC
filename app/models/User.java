d4ed97cb7f19b6467170b6983b00f1bcac76a9fc
cperez3 <chrisv.perez@gmail.com>
2016-03-21 15:24:57 -0400

    Started making user model

---
+++ app/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+/**
+ * Created by chris_000 on 3/21/2016.
+ */
+public class User {
+  public String name;
+  public String email;
+  public String password;
+  public String sex;
+  public int age;
+  public int weight;
+
+}
