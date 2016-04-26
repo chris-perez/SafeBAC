ba4396ed6c4f5ad96f296b7af530e29d43d88ac5
Chris <chrisv.perez@gmail.com>
2016-04-25 22:56:09 -0400

    fixed tests

--- test/UsersTest.java
+++ test/UsersTest.java
@@ -1,10 +1,14 @@
 import com.avaje.ebean.Ebean;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import controllers.Users;
+import models.Drink;
 import models.User;
+import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Test;
 import play.Logger;
+import play.api.libs.Crypto;
 import play.libs.Json;
 import play.mvc.Result;
 
@@ -24,16 +28,20 @@ public class UsersTest {
   String TEST_SEX = "male";
   int TEST_WEIGHT = 160;
   int TEST_AGE = 18;
-  String TEST_AUTH_TOKEN = "";
+  String TEST_AUTH_TOKEN = Users.genID();
 
   String CHANGE_NAME = "Keith Stone";
   String CHANGE_SEX = "male";
   int CHANGE_WEIGHT = 200;
   int CHANGE_AGE = 21;
 
+  ObjectNode request;
+  JsonNode content;
+  Result result;
+
   @Test
   public void testBAC() {
-    running(fakeApplication(), new Runnable() {
+    running(fakeApplication(inMemoryDatabase()), new Runnable() {
 
       private void checkBAC(){
         Assert.assertEquals(9,User.calculateBAC(115,"id","female",2,3));
@@ -50,25 +58,22 @@ public class UsersTest {
     });
   }
 
+  private void validateUser(JsonNode content) {
+    assertThat(content.has("id")).isTrue();
+    assertThat(content.has("authID")).isTrue();
+    assertThat(content.get("authID")).isNotEqualTo(null);
+    assertThat(content.has("name")).isTrue();
+    assertThat(content.has("email")).isTrue();
+    assertThat(content.has("sex")).isTrue();
+    assertThat(content.has("weight")).isTrue();
+    assertThat(content.has("birthDate")).isTrue();
+  }
+
   @Test
-  public void userTest() {
-    running(fakeApplication(), new Runnable() {
-      ObjectNode request;
-      JsonNode content;
-      Result result;
-
-      private void validateUser(JsonNode content) {
-        assertThat(content.has("id")).isTrue();
-        assertThat(content.has("authID")).isTrue();
-        assertThat(content.get("authID")).isNotEqualTo(null);
-        assertThat(content.has("name")).isTrue();
-        assertThat(content.has("email")).isTrue();
-        assertThat(content.has("sex")).isTrue();
-        assertThat(content.has("weight")).isTrue();
-        assertThat(content.has("birthDate")).isTrue();
-      }
-
-      private void createUser() {
+  public void createUser() {
+    running(fakeApplication(inMemoryDatabase()), new Runnable() {
+      @Override
+      public void run() {
         // Create User
         request = Json.newObject()
             .put("email", TEST_EMAIL)
@@ -85,8 +90,15 @@ public class UsersTest {
         content = Json.parse(contentAsString(result));
         validateUser(content);
       }
+    });
+  }
 
-      private void loginUser() {
+  @Test
+  public void loginUser() {
+    running(fakeApplication(inMemoryDatabase()), new Runnable() {
+      @Override
+      public void run() {
+        User u = new User("name", TEST_EMAIL, Crypto.sign(TEST_PASSWORD), TEST_SEX, DateTime.now(), TEST_WEIGHT, TEST_AUTH_TOKEN);
         // Login
         request = Json.newObject()
             .put("email", TEST_EMAIL)
@@ -97,41 +109,43 @@ public class UsersTest {
         assertThat(status(result)).isEqualTo(OK);
         content = Json.parse(contentAsString(result));
         validateUser(content);
-        TEST_AUTH_TOKEN = content.get("authID").asText();
       }
+    });
+  }
 
-      private void updateProfile() {
+  @Test
+  public void updateProfile() {
+    running(fakeApplication(inMemoryDatabase()), new Runnable() {
+      @Override
+      public void run() {
+        User u = new User("name", TEST_EMAIL, TEST_PASSWORD, TEST_SEX, DateTime.now(), TEST_WEIGHT, TEST_AUTH_TOKEN);
         // Login
         request = Json.newObject()
-                .put("email", TEST_EMAIL)
-                .put("name", CHANGE_NAME)
-                .put("age", CHANGE_AGE)
-                .put("sex", CHANGE_SEX)
-                .put("weight", CHANGE_WEIGHT);
+            .put("email", TEST_EMAIL)
+            .put("name", CHANGE_NAME)
+            .put("age", CHANGE_AGE)
+            .put("sex", CHANGE_SEX)
+            .put("weight", CHANGE_WEIGHT);
         result = callAction(controllers.routes.ref.Users.updateProfile(),
-                fakeRequest().withHeader("X-Auth-Token", TEST_AUTH_TOKEN).withJsonBody(request));
-        Logger.info("Login Result: " + contentAsString(result));
+            fakeRequest().withHeader("X-Auth-Token", TEST_AUTH_TOKEN).withJsonBody(request));
+        Logger.info("Update Profile Result: " + contentAsString(result));
         assertThat(status(result)).isEqualTo(OK);
       }
+    });
+  }
 
-      private void logoutUser() {
+  @Test
+  public void logoutUser() {
+    running(fakeApplication(inMemoryDatabase()), new Runnable() {
+      @Override
+      public void run() {
+        User u = new User("name", TEST_EMAIL, TEST_PASSWORD, TEST_SEX, DateTime.now(), TEST_WEIGHT, TEST_AUTH_TOKEN);
         // Logout
         result = callAction(controllers.routes.ref.Users.logoutUser(),
             fakeRequest().withHeader("X-Auth-Token", TEST_AUTH_TOKEN));
         assertThat(status(result)).isEqualTo(NO_CONTENT);
-      }
-
-      @Override
-      public void run() {
-        Ebean.beginTransaction();
-        try {
-          createUser();
-          loginUser();
-          updateProfile();
-          logoutUser();
-        } finally {
-          Ebean.rollbackTransaction();
-        }
+        User user = User.find.where().eq("email", TEST_EMAIL).findUnique();
+        assertThat(user.getAuthID()).isNull();
       }
     });
   }
